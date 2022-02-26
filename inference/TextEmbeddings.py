2400e04be74b2ae064189a47497b67bca52619a4
lethologicoding <lethologicoding@gmail.com>
2022-02-26 18:43:25 -0500

    swapped out sentence embeddings model

--- inference/TextEmbeddings.py
+++ inference/TextEmbeddings.py
@@ -6,7 +6,8 @@ def get_bert_sentence_embeddings(df = pd.Series):
     '''
     Uses bert sentence embeddings to transform raw text into embeddings
     '''
-    sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
+    # sbert_model = SentenceTransformer('bert-base-nli-mean-tokens')
+    sbert_model = SentenceTransformer('all-MiniLM-L12-v2')
     sentence_embeddings = sbert_model.encode(df)
     
     return sentence_embeddings
